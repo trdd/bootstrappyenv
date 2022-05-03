10a90874363e9fe92fc6d3092f0732d4b5259213
Tobias Ritschl <t.ritschel@posteo.de>
2022-05-03 11:15:35 +0200

    simple bootstrap script to install pyenv on ubuntu

---
+++ installpyenv.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+git clone https://github.com/pyenv/pyenv.git ~/.pyenv
+cd ~/.pyenv && src/configure && make -C src
+
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
+echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
+echo 'eval "$(pyenv init -)"' >> ~/.bashrc
+
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.profile
+echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.profile
+echo 'eval "$(pyenv init -)"' >> ~/.profile
+
+exec "$SHELL"
