fca586264498da80341297fb607f2c4934574e80
Tobias Ritschl <t.ritschel@posteo.de>
2022-05-03 11:16:48 +0200

    add install of build deps:wq

--- installpyenv.sh
+++ installpyenv.sh
@@ -11,3 +11,7 @@ echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.
 echo 'eval "$(pyenv init -)"' >> ~/.profile
 
 exec "$SHELL"
+
+sudo apt-get update; sudo apt-get install make build-essential libssl-dev zlib1g-dev \
+    libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm \
+    libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
